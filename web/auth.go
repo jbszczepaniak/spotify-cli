18f329f3d1132968612f99dc5ac046b6b5d8e6fe
Jedrzej <jbszczepaniak@gmail.com>
2019-07-30 19:04:52 +0200

    Split handler

--- web/auth.go
+++ web/auth.go
@@ -1,1 +1,83 @@
 package web
+
+import (
+	"fmt"
+	"github.com/zmb3/spotify"
+	"golang.org/x/oauth2"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+type AuthHandler struct {
+	// Client is created as the result of Spotify backend calling auth callback,
+	// as soon as spotify Client is created, the rest of the application can use
+	// is to call any spotify apis.
+	Client chan *spotify.Client
+
+	// State is random string used in order to authenticate Client. It is used to
+	// generate spotify backend URL to which user will be redirected. After user
+	// comes back to the application (to the auth callback) it is used to verify
+	// message received from the spotify backend.
+	State string
+
+	// used by auth callback to verify message from spotify backend
+	// and to create a spotify Client.
+	Authenticator SpotifyAuthenticatorInterface
+}
+
+
+// authCallback is a function to by Spotify upon successful
+// user login at their site
+func (s *AuthHandler) AuthCallback(w http.ResponseWriter, r *http.Request) {
+	token, err := s.Authenticator.Token(s.State, r)
+	if err != nil {
+		errMsg := fmt.Sprintf("Could not get token, error: %v", err)
+		http.Error(w, errMsg, http.StatusNotFound)
+		log.Print(errMsg)
+		return
+	}
+
+	client := s.Authenticator.NewClient(token)
+	s.Client <- &client
+
+	http.Redirect(w, r, fmt.Sprintf("http://localhost:8888/player?token=%s", token.AccessToken), 301)
+}
+
+
+
+type SpotifyAuthenticatorInterface interface {
+	AuthURL(string) string
+	Token(string, *http.Request) (*oauth2.Token, error)
+	NewClient(*oauth2.Token) spotify.Client
+}
+
+func NewSpotifyAuthenticator() SpotifyAuthenticatorInterface {
+	envKeys := []string{"SPOTIFY_CLIENT_ID", "SPOTIFY_SECRET"}
+	envVars := map[string]string{}
+	for _, key := range envKeys {
+		v := os.Getenv(key)
+		if v == "" {
+			log.Fatalf("Quiting, there is no %s environment variable.", key)
+		}
+		envVars[key] = v
+	}
+
+	redirectURI := url.URL{Scheme: "http", Host: "localhost:8888", Path: "/spotify-cli"}
+
+	auth := spotify.NewAuthenticator(
+		redirectURI.String(),
+		spotify.ScopeUserReadPrivate,
+		spotify.ScopeUserReadCurrentlyPlaying,
+		spotify.ScopeUserReadPlaybackState,
+		spotify.ScopeUserModifyPlaybackState,
+		spotify.ScopeUserLibraryRead,
+		// Used for Web Playback SDK
+		"streaming",
+		spotify.ScopeUserReadBirthdate,
+		spotify.ScopeUserReadEmail,
+	)
+	auth.SetAuthInfo(envVars["SPOTIFY_CLIENT_ID"], envVars["SPOTIFY_SECRET"])
+	return auth
+}
--- web/websocket.go
+++ web/websocket.go
@@ -1,1 +1,77 @@
 package web
+
+import (
+	"encoding/json"
+	"github.com/gorilla/websocket"
+	"github.com/zmb3/spotify"
+	"log"
+	"net/http"
+	"time"
+)
+
+type WebsocketHandler struct {
+	// signal sent by the application to the websocket handler that triggers tear down
+	// of websocket.
+	PlayerShutdown chan bool
+
+	// as soon as web player is ready it sends player device id as the very first
+	// message on the websocket connection. Application may want to use this ID in
+	// order to control on which player to play music (the one that it created, or
+	// maybe on the other device that is also working)
+	PlayerDeviceID chan spotify.ID
+
+	// each time web player changes it's State it sens information about what is
+	// currently played on the websocket
+	PlayerStateChange chan *WebPlaybackState
+}
+
+type WebPlaybackReadyDevice struct {
+	DeviceId string
+}
+
+type WebPlaybackState struct {
+	CurrentTrackName  string
+	CurrentAlbumName  string
+	CurrentArtistName string
+}
+
+func (s *WebsocketHandler) Handle(w http.ResponseWriter, r *http.Request) {
+	upgrader := websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	var v WebPlaybackReadyDevice
+	_, message, err := conn.ReadMessage()
+	err = json.Unmarshal(message, &v)
+	if err != nil {
+		log.Fatalf("could not unmarshall message from web player, err: %v", err)
+	}
+	deviceId := spotify.ID(v.DeviceId)
+	log.Printf("web player is ready, it's ID is %s", deviceId.String())
+	s.PlayerDeviceID <- deviceId
+
+	go func() {
+		for range time.Tick(500 * time.Millisecond) {
+			var state WebPlaybackState
+			_, message, err = conn.ReadMessage()
+			err = json.Unmarshal(message, &state)
+			if err != nil {
+				log.Printf("could not Unmarshall message: %s, err: %s", message, err)
+			}
+			s.PlayerStateChange <- &state
+		}
+	}()
+
+	<-s.PlayerShutdown
+	err = conn.WriteJSON("{\"close\": true}")
+	if err != nil {
+		log.Printf("could not close connection, err %v", err)
+	}
+
+}
